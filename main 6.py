65fcf8653cc10e40e6f5d32cb6437e2ef68d85d0
Ongenix <96583029+Ongenix@users.noreply.github.com>
2022-05-12 22:42:13 -0600

    Main file

---
+++ main 6.py
@@ -0,0 +1,47 @@
+import os
+from keep_alive import keep_alive
+from discord.ext import commands
+
+__author__ = 'Ongenix'
+
+bot = commands.Bot(
+	command_prefix="!",
+	case_insensitive=True
+)
+
+bot.author_id = 0
+
+
+delete_anti_ping_message = True
+wait_time_to_delete_anti_ping_message = '10 seconds'
+#this will make the bot unsable while waiting
+delete_ping_message = True
+log_pingers = False
+#this will alert any ghost ping bots
+
+
+wait_time_to_delete_anti_ping_message = wait_time_to_delete_anti_ping_message.replace(' seconds','');wait_time_to_delete_anti_ping_message=int(wait_time_to_delete_anti_ping_message)
+@bot.event 
+async def on_message(ctx):
+  if '@' in ctx.content and ctx.author != bot.user:
+    if (ctx.content).find(' ') != -1:
+      x = ((ctx.content).split(' ')[0])
+    else:
+      x = ctx.content
+    x=x.replace('@','')
+    if delete_anti_ping_message:
+      await ctx.channel.send(f'User {ctx.author} was trying to ping {x}!', delete_after=wait_time_to_delete_anti_ping_message)
+    else:
+      await ctx.channel.send(f'User {ctx.author} was trying to ping {x}!')
+    if log_pingers:
+      f = open('log.txt','a')
+      f.write('\n')
+      f.write(f'[+] {ctx.author} - {(ctx.content)}')
+      f.close()
+    if delete_ping_message:
+      msg = await ctx.channel.fetch_message(ctx.id)
+      await msg.delete()
+
+keep_alive()
+token = 'put your bot token here'
+bot.run(token)
